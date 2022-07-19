26b8273b6ef0a508c747ecb242b9b0a5f6684fee
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2022-07-18 21:41:03 -0400

    ajout d'une classe CSS

--- theme-31w-exer1/front-page.php
+++ theme-31w-exer1/front-page.php
@@ -1,25 +1,25 @@
-<?php get_header(); ?> 
+<?php get_header(); ?>
 <section class='site__main '>
-<!--<h1>Mon premier modèle Wordpress</h1> page d'accueil-->
-<?php
-if (have_posts()):
-    while(have_posts()) : the_post(); ?>
-    <div class="main">
-        
-    
-        <h2><?php the_title(); ?></h2><br>
-        <?php $lien = " ... <a class='bouton' href='" 
-                . get_permalink()  
-                . "'>"
-                . substr(get_the_title(),0,8) 
-                ."</a>"; 
+    <!--<h1>Mon premier modèle Wordpress</h1> page d'accueil-->
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post(); ?>
+            <div class="main">
 
-?>
 
-<p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
-</div>
+                <h2><?php the_title(); ?></h2><br>
+                <?php $lien = " ... <a class='bouton' href='"
+                    . get_permalink()
+                    . "'>"
+                    . substr(get_the_title(), 0, 8)
+                    . "</a>";
 
-    <?php endwhile; ?>
-<?php endif; ?>    
+                ?>
+
+                <p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
+            </div>
+
+        <?php endwhile; ?>
+    <?php endif; ?>
 </section>
 <?php get_footer(); ?>
