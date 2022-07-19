f3400dc588a4df3998705e6b2c9322fe38e392b7
LaetiJSTE <98364569+LaetiJSTE@users.noreply.github.com>
2022-07-18 21:41:38 -0400

    ajout d'une classe CSS

--- theme-31w-exer1/header.php
+++ theme-31w-exer1/header.php
@@ -19,6 +19,7 @@
 <section class="site">
         <header class="site__header">
             <section class="site__branding">
+              
                 <h1>
                     <a href="<?= esc_url( home_url( '/' ) ) ?>"><?= bloginfo( 'name' ); ?></a>
                 </h1>
